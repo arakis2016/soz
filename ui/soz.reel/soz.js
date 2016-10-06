d94feff6866cbe74b1212095ca3967d8bd33de2b
arakis2016 <noreply>
2016-10-06 14:51:39 +0000

    Add component soz

---
+++ ui/soz.reel/soz.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/soz.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Soz
+ * @extends Component
+ */
+exports.Soz = Component.specialize(/** @lends Soz# */ {
+    constructor: {
+        value: function Soz() {
+            this.super();
+        }
+    }
+});
